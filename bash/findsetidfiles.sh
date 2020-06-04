e168ed6e3054b5bb2ad010540ed9e0e233412821
manish syangdan <manishlama362@gmail.com>
2020-06-04 08:39:58 -0700

    added comments

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -24,13 +24,40 @@ echo "============="
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo ""
 
+
+#task 1
+#script is modified such that it displays setgid file on second listing.
 # Displaying setgid files
 echo "Setgid files:"
+#setgid is a special file permission that applies for executable file
+#echo prints the exact output as enclosed in""
 echo "=============="
+#echo prints the exact output as enclosed in""
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 4
+#only display files with permissions set to 2000
+#find command is used to search for files.
+# / signifies home or root directory
+#2> /dev/null redirects to /dev/null if error is encountered.
+#sort -k is used to sort file on basis of key.
+# | pipe is used to combine two or more commands.
 echo ""
+#echo prints the exact output as enclosed in""
 
+
+#task 2
+#script is modified to display 10 largest regular files in system
 # Displaying latest files
 echo "Largest files:"
+#echo prints the exact output as enclosed in""
 echo "============="
+#echo prints the exact output as enclosed in""
 find / -type f -exec du -Sh {} + 2>/dev/null | sort -rh | head -n 10
+#find command is used to search for files.
+# / signifies home or root directory
+#2> /dev/null redirects to /dev/null if error is encountered.
+#du command estimates disk usage
+#du -sh compares human readable numbers and shows total of for each argument.
+#sort -rh sorts value in human readable format and reverse results.
+#{} are used to summarize disk usage of each file
+# | pipe is used to combine two or more commands.
+# head -n 10 displays first 10 files
